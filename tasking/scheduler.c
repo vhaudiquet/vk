570ace8927fbb2c59dccc0b94bf965d6a8c276b3
Valou3433 <vhaudiquet343@hotmail.fr>
2018-02-28 19:01:22 +0100

    sched-redesign: added comments

--- tasking/scheduler.c
+++ tasking/scheduler.c
@@ -27,12 +27,19 @@ list_entry_t* irq_list[21] = {0};
 list_entry_t* wait_list = 0;
 //mutex_t wait_mutex = 0;
 
+/*
+* Initializes the data structures needed by the scheduler
+*/
 void scheduler_init()
 {
     p_ready_queue = queue_init(10);
+    //kmalloc(4);
     //wait_mutex = mutex_alloc(); *wait_mutex = 0;
 }
 
+/*
+* Start the scheduler, by activating interrupts
+*/
 void scheduler_start()
 {
     kprintf("Starting scheduler...");
@@ -41,6 +48,9 @@ void scheduler_start()
     vga_text_okmsg();
 }
 
+/*
+* Add a process to the scheduler
+*/
 void scheduler_add_process(process_t* process)
 {
     if((process != idle_process) && (process->status == PROCESS_STATUS_RUNNING)) return;
@@ -48,6 +58,9 @@ void scheduler_add_process(process_t* process)
     queue_add(p_ready_queue, process);
 }
 
+/*
+* Remove an active process from the scheduler
+*/
 void scheduler_remove_process(process_t* process)
 {
     process->status = PROCESS_STATUS_ASLEEP;
@@ -61,55 +74,71 @@ void scheduler_remove_process(process_t* process)
         current_process->gregs.edi = current_process->gregs.esi = current_process->ebp = 0;
         current_process->sregs.ds = current_process->sregs.es = current_process->sregs.fs = current_process->sregs.gs = current_process->sregs.ss = 0x10;
         current_process->sregs.cs = 0x08;
-        asm("mov %%esp, %%eax":"=a"(current_process->esp));
-        current_process->eip = (u32) &&rmv; //(scheduler_remove_process+0xe8);//(c0108133);
+        current_process->eip = (u32) &&rmv;
 
-        current_process = 0;
         if(p_ready_queue->rear < p_ready_queue->front)
         {
             scheduler_add_process(idle_process);
         }
+        
+        __asm__ __volatile__("mov %%esp, %%eax":"=a"(current_process->esp)::); //save esp at last moment
+        current_process = 0; //remove current process so that it doesnt get re-saved and put back on queue
+        
         asm("int $32"); //call clock int to schedule
         rmv: return;
     }
     else queue_remove(p_ready_queue, process);
 }
 
+/*
+* Put a process to sleep, either for an ammount of time or to wait an IRQ
+*/
 void scheduler_wait_process(process_t* process, u8 sleep_reason, u16 sleep_data, u16 wait_time)
 {
     //if(mutex_lock(wait_mutex) != ERROR_NONE) fatal_kernel_error("Could not lock wait mutex", "SCHEDULER_WAIT_PROCESS");
 
+    /* if we need to sleep a certain ammount of time */
     if(wait_time && ((sleep_reason == SLEEP_WAIT_IRQ) | (sleep_reason == SLEEP_TIME)))
     {
-        //kprintf("wait sleep\n");
         list_entry_t* ptr = wait_list;
         list_entry_t* last = 0;
         u32 cumulated_time = wait_time;
+
+        /* we have to calculate the right place of this process in the list by calculating cumulated time */
         while(ptr && cumulated_time)
         {
+            //getting the time of this list element
             u32 element_time = ((u32*)ptr->element)[1];
+            //if the cumulated time of this list element is less than our process, we break : the entry needs to be right before that
             if(cumulated_time < element_time) break;
+            //recalculate cumulated time of our process
             cumulated_time -= element_time; 
+            //iterate through the list
             last = ptr;
             ptr = ptr->next;
         }
 
-        //kprintf("found place in queue.\n");
+        //found a place in the list, allocate entry and put it
         list_entry_t* entry = kmalloc(sizeof(list_entry_t));
         if(!last) {wait_list = entry; ptr = 0;}
         else {last->next = entry;}
-        
         entry->next = ptr;
+
+        //allocate space for time/process and put it into the entry
         uintptr_t* element = kmalloc(sizeof(uintptr_t)*2);
         element[0] = (uintptr_t) process;
         element[1] = wait_time;
         entry->element = element;
     }
 
+    /* if we need to wait for an irq */
     if((sleep_data <= 20) && (sleep_reason == SLEEP_WAIT_IRQ))
     {
+        //pointer to the list
         list_entry_t** ptr = &irq_list[sleep_data];
+        //while there are elements in the list, we iterate through
         while(*ptr) ptr = &((*ptr)->next);
+        //we allocate space and set entry data (our process)
         (*ptr) = kmalloc(sizeof(list_entry_t));
         (*ptr)->next = 0;
         (*ptr)->element = process;
@@ -119,14 +148,20 @@ void scheduler_wait_process(process_t* process, u8 sleep_reason, u16 sleep_data,
     scheduler_remove_process(process);
 }
 
+/*
+* Update the wait_list of processes, to decrease elapsed time (called by schedule())
+*/
 void scheduler_sleep_update()
 {
+    //check if there are processes on the list
     if(!wait_list) return;
     //if(mutex_lock(wait_mutex) != ERROR_NONE) return;
 
     list_entry_t* ptr = wait_list;
     u32* element = ptr->element;
+    //remove 55 ms to the top element (schedule is executed ~ every 55 ms)
     if(element[1] > 55) element[1] -= 55;
+    //one element as reach 0, put it back in queue
     else
     {
         wait_list = 0;
@@ -143,22 +178,26 @@ void scheduler_sleep_update()
 
             element = ptr->element;
         }
-        while(!element[1]);
+        while(!element[1]); //while the elements have a value of 0, we add them on queue
     }
 
     //mutex_unlock(wait_mutex);
 }
 
+/*
+* Wake up every process that needed to be on irq x (called by every irq)
+*/
 void scheduler_irq_wakeup(u32 irq)
 {
     if(!irq_list[irq]) return;
 
+    /* we need to remove/free every element of the list and add every process to the scheduler */
     scheduler_add_process((process_t*) irq_list[irq]->element);
     list_entry_t* ptr = irq_list[irq]->next;
     kfree(irq_list[irq]); irq_list[irq] = 0;
     while(ptr)
     {
-        scheduler_add_process((process_t*) irq_list[irq]->element);
+        scheduler_add_process((process_t*) ptr->element);
         list_entry_t* to_free = ptr;
         ptr = ptr->next;
         kfree(to_free);
